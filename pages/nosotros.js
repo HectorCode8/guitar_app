87c7446dcfeed5931e0563f2c90db22004888449
Hector Haro H <hector8.haro@gmail.com>
2022-05-02 23:51:12 -0500

    and more more more css

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import Image from 'next/image'
 import Layout from '../components/Layout'
+import styles from '../styles/Nosotros.module.css'
 
 
 const Nosotros = () => {
@@ -7,7 +8,46 @@ const Nosotros = () => {
     <Layout
         pagina='Nosotros'
     >
-        <h1>Desde Nosotros</h1>
+        <main className='contenedor'>
+          <h2 className='heading'>Nosotros</h2>
+          <div className={styles.contenido}>
+
+            <Image layout="responsive" 
+                    src='/img/nosotros.jpg' 
+                    alt='Nosotros' 
+                    width={600} 
+                    height={450} />
+
+            <div>
+              <p>
+              Suspendisse pretium tellus quis viverra luctus.
+               Cras eleifend mi a ex finibus commodo.
+                Donec quis nibh volutpat, rutrum tortor id, scelerisque ligula.
+                 Pellentesque sed consectetur ex. Sed imperdiet egestas sapien ac congue.
+                  Curabitur feugiat vel dui id ullamcorper. Morbi interdum orci purus.
+                   Aenean consequat ante et enim hendrerit, eget volutpat erat interdum.
+                    Vestibulum lacinia erat nisi, non ornare enim finibus et.
+                     Suspendisse varius urna consectetur nunc mattis, quis bibendum tortor fermentum.
+                      Nulla nec ultrices dolor. Donec dapibus sem et tortor convallis posuere.
+                       Suspendisse iaculis est eget mattis pellentesque.
+                        Nam porttitor sem felis, vel vestibulum est rutrum vitae.
+              </p>
+              <p>
+              Suspendisse pretium tellus quis viverra luctus.
+               Cras eleifend mi a ex finibus commodo.
+                Donec quis nibh volutpat, rutrum tortor id, scelerisque ligula.
+                 Pellentesque sed consectetur ex. Sed imperdiet egestas sapien ac congue.
+                  Curabitur feugiat vel dui id ullamcorper. Morbi interdum orci purus.
+                   Aenean consequat ante et enim hendrerit, eget volutpat erat interdum.
+                    Vestibulum lacinia erat nisi, non ornare enim finibus et.
+                     Suspendisse varius urna consectetur nunc mattis, quis bibendum tortor fermentum.
+                      Nulla nec ultrices dolor. Donec dapibus sem et tortor convallis posuere.
+                       Suspendisse iaculis est eget mattis pellentesque.
+                        Nam porttitor sem felis, vel vestibulum est rutrum vitae.
+              </p>
+            </div>
+          </div>
+        </main>
     </Layout>
   )
 }
