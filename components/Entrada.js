d41aebd883565069f70b4a68851a910bb65a47ce
Hector Haro H <hector8.haro@gmail.com>
2022-05-04 22:06:31 -0500

    add helpers and more logic

--- components/Entrada.js
+++ components/Entrada.js
@@ -1,9 +1,18 @@
+import { formatearFecha } from '../helpers'
+import Link from 'next/link'
 
 const Entrada = ({entrada}) => {
     const {titulo, resumen, imagen, published_at, id} = entrada
  return (
     <article>
-        <h1>{titulo}</h1>
+        <div>
+            <h1>{titulo}</h1>
+            <p>{formatearFecha(published_at)}</p>
+            <p>{resumen}</p>
+            <Link href={`/blog/${id}`}>
+                Leer Entrada
+            </Link>
+        </div>
     </article>
   )
 }
