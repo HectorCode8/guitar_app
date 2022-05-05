92c03b80419684e0d19f3593ac85cf5f5800a312
Hector Haro H <hector8.haro@gmail.com>
2022-05-04 21:40:01 -0500

    add endpoint to my app

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,19 @@
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 import Layout from '../components/Layout'
 
 const Blog = () => {
 
   useEffect(() => {
-    
-  })
+    const consultarAPI = async () => {
+      const url = 'http://localhost:1337/blogs'
+      const respuesta = await fetch(url)
+      const resultado = await respuesta.json()
+
+      console.log(resultado)
+    }
+    consultarAPI()
+  }, [])
+
   return (
     <Layout
         pagina='Blog'
