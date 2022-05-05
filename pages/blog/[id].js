f476e951ab7cfdb8a48fc8162e2acbd1a9077547
Hector Haro H <hector8.haro@gmail.com>
2022-05-04 23:02:53 -0500

    add getServerSideProps

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,10 +1,20 @@
+import {useRouter} from 'next/router'
 
 const EntradaBlog = () => {
+    const router = useRouter()
   return (
     <div>
         <h1>Desde Entrada Blog</h1>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+    return{
+        props: {
+            
+        }
+    }
 }
 
 export default EntradaBlog
