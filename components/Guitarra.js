d10ed7746ed3344ebfeb6fa3fc70250f66b98c21
Hector Haro H <hector8.haro@gmail.com>
2022-05-06 15:33:31 -0500

    Finish css from guitarras

--- components/Guitarra.js
+++ components/Guitarra.js
@@ -1,24 +1,27 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from '../styles/Guitarra.module.css'
 
 const Guitarra = ({guitarra}) => {
     const {descripcion, imagen, nombre, precio, url} = guitarra
   return (
-    <div>
+    <div className={styles.guitarra}>
        <Image 
             layout='responsive' 
-            width={500} 
+            width={180} 
             height={350} 
             src={imagen.url} 
             alt={`Imagen Guitarra ${nombre}`} 
             priority={true}
         />
-        <div>
+        <div className={styles.contenido}>
             <h3>{nombre}</h3>
-            <p>{descripcion}</p>
-            <p>Precio: ${precio}</p>
+            <p className={styles.descripcion}>{descripcion}</p>
+            <p className={styles.precio}>${precio}</p>
             <Link href={`/guitarras/${url}`}>
-                <a>Ver detalles</a>
+                <a className={styles.enlace}>
+                    Ver Producto
+                </a>
             </Link>
         </div>
     </div>
