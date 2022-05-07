88f7a8b4a9fd8aff93fa8262361f76a869c39794
Hector Haro H <hector8.haro@gmail.com>
2022-05-06 23:31:22 -0500

    CSS to the Courses block

--- components/Curso.js
+++ components/Curso.js
@@ -1,3 +1,4 @@
+import styles from '../styles/Curso.module.css'
 
 const Curso = ({curso}) => {
     const {contenido, titulo, imagen} = curso
@@ -11,6 +12,15 @@ const Curso = ({curso}) => {
                 <a href="#">Más Informacion</a>
             </div>
         </div>
+        <style jsx>{`
+            section {
+                padding: 10rem 0;
+                margin-top: 10rem;
+                background-image: linear-gradient(to right, rgb(0 0 0 / .65), rgb(0 0 0 / .7)), url(${imagen.url});
+                background-size: cover;
+                background-position: 50%;
+            }
+        `}</style>
     </section>
   )
 }
