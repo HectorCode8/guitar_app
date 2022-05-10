ef50205efa051b65f4c83727ca3499e7a891f075
Hector Haro H <hector8.haro@gmail.com>
2022-05-09 21:07:12 -0500

    add global state to carristo.js

--- pages/carrito.js
+++ pages/carrito.js
@@ -2,7 +2,8 @@ import Layout from '../components/Layout'
 import styles from '../styles/Carrito.module.css'
 
 
-const Carrito = () => {
+const Carrito = ({carrito}) => {
+    console.log(carrito)
   return (
     <Layout pagina={'Carrito de Compras'}> 
         <h1 className="heading">Carrito de Compras</h1>
